a663928cfdefb17aafc5d43fd92571f54b3d413e
ychenchen <ycc@itcast.cn>
2025-06-30 17:14:10 +0800

    小程序端微实战-参观预约接口

--- zzyl-nursing-platform/src/main/java/com/zzyl/nursing/controller/member/MemberReservationController.java
+++ zzyl-nursing-platform/src/main/java/com/zzyl/nursing/controller/member/MemberReservationController.java
@@ -1,13 +1,22 @@
 package com.zzyl.nursing.controller.member;
 
 import com.zzyl.common.core.controller.BaseController;
+import com.zzyl.common.core.domain.AjaxResult;
 import com.zzyl.common.core.domain.R;
+import com.zzyl.common.core.page.TableDataInfo;
 import com.zzyl.common.utils.UserThreadLocal;
+import com.zzyl.nursing.domain.Reservation;
+import com.zzyl.nursing.dto.ReservationDto;
+import com.zzyl.nursing.service.IReservationService;
+import com.zzyl.nursing.vo.TimeCountVo;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 预约信息Controller
@@ -19,11 +28,56 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags =  "预约信息相关接口")
 public class MemberReservationController extends BaseController
 {
+    @Autowired
+    private IReservationService reservationService;
+
     @GetMapping("/cancelled-count")
     @ApiOperation("查询取消预约数量")
     public R<Integer> getCancelledReservationCount() {
         Long userId = UserThreadLocal.getUserId();
-        System.out.println("------" + userId);
-        return R.ok(1);
+        int count = reservationService.getCancelledCount(userId);
+        return R.ok(count);
+    }
+
+    @GetMapping("/countByTime")
+    @ApiOperation("查询每个时间段剩余预约次数")
+    public R<List<TimeCountVo>> countReservationsForEachTimeWithinTimeRange(Long time) {
+        List<TimeCountVo> list = reservationService.countReservationsForTime(time);
+        return R.ok(list);
+    }
+
+    /**
+     * 新增预约信息
+     */
+    @PostMapping
+    @ApiOperation("新增预约信息")
+    public AjaxResult add(@RequestBody ReservationDto reservationDto) {
+        return toAjax(reservationService.insertReservation(reservationDto));
+    }
+
+    /*
+     *分页查询增加预约人姓名，手机号，状态，类型的查询条件
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询预约")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum", value = "页码", required = true, dataType = "int", paramType = "query"),
+            @ApiImplicitParam(name = "pageSize", value = "每页数量", required = true, dataType = "int", paramType = "query"),
+            @ApiImplicitParam(name = "status", value = "状态", dataType = "int", paramType = "query"),
+    })
+    public R<TableDataInfo<Reservation>> findByPage(@RequestParam(defaultValue = "1") int pageNum,
+                                                    @RequestParam(defaultValue = "10") int pageSize,
+                                                    @RequestParam(required = false) Integer status) {
+        TableDataInfo<Reservation> tableDataInfo = reservationService.selectByPage(pageNum, pageSize, status);
+        return R.ok(tableDataInfo);
+    }
+
+    /**
+     * 取消预约
+     */
+    @PutMapping("/{id}/cancel")
+    @ApiOperation("取消预约")
+    public AjaxResult cancel(@PathVariable Long id) {
+        return toAjax(reservationService.cancelReservation(id));
     }
 }
---
+++ zzyl-nursing-platform/src/main/java/com/zzyl/nursing/service/IReservationService.java
@@ -0,0 +1,60 @@
+package com.zzyl.nursing.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zzyl.common.core.page.TableDataInfo;
+import com.zzyl.nursing.domain.Reservation;
+import com.zzyl.nursing.dto.ReservationDto;
+import com.zzyl.nursing.vo.TimeCountVo;
+
+import java.util.List;
+
+/**
+ * 预约信息Service接口
+ * 
+ * @author alexis
+ * @date 2024-11-11
+ */
+public interface IReservationService extends IService<Reservation>
+{
+    /**
+     * 查询取消预约次数
+     *
+     * @param userId
+     * @return
+     */
+    int getCancelledCount(Long userId);
+
+    /**
+     * 新增预约信息
+     *
+     * @param reservationDto 预约信息
+     * @return 结果
+     */
+    int insertReservation(ReservationDto reservationDto);
+
+    /**
+     * 查询每个时间段剩余预约次数
+     *
+     * @return
+     */
+    List<TimeCountVo> countReservationsForTime(Long time);
+
+    /**
+     * 分页查询预约信息
+     *
+     * @param pageNum
+     * @param pageSize
+     * @param status
+     * @return
+     */
+    TableDataInfo<Reservation> selectByPage(int pageNum, int pageSize, Integer status);
+
+    /**
+     * 取消预约
+     *
+     * @param id
+     * @return
+     */
+    int cancelReservation(Long id);
+
+}
